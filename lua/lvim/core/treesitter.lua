fe76ecd8a4d7a307dc6bb408ffdfb603836ae7b1
adkun <adgnn@foxmail.com>
2023-03-27 14:53:22 +0800

    feature: [rainbow]

--- lua/lvim/core/treesitter.lua
+++ lua/lvim/core/treesitter.lua
@@ -82,9 +82,11 @@ function M.config()
             },
         },
         rainbow = {
-            enable = false,
+            enable = true,
             extended_mode = true,  -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
             max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+            query = 'rainbow-parens',
+            strategy = require('ts-rainbow').strategy.global,
         },
     }
 end
